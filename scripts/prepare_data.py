964db8d10b95d0a60bd5e5527b6b8f076bb2de85
martin <villasen@unlv.nevada.edu>
2019-11-16 18:10:15 -0800

    adding speech classification test scripts

---
+++ scripts/prepare_data.py
@@ -0,0 +1,166 @@
+import keras
+from keras import utils
+import numpy as np
+
+import tensorflow
+import scipy
+import os, shutil
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from six.moves import urllib
+import sys
+import tarfile
+import os.path
+from os import path
+from tensorflow.python.ops import io_ops
+from tensorflow.contrib.framework.python.ops import audio_ops as contrib_audio
+import matplotlib.pyplot as plt
+import subprocess
+from timeit import default_timer as timer
+
+
+
+from numpy  import array
+from tensorflow.contrib.quantize.python import fold_batch_norms
+from tensorflow.contrib.quantize.python import quantize
+from tensorflow.python.framework import ops
+print(tf.__version__)
+
+
+tf.enable_eager_execution()
+
+###   variables
+MFCC=10
+X_train_max=2.580736630323992
+X_train_min=-6.401088510838375
+
+
+########### FUNCTIONS
+
+def predict(records, n):
+	max = records[0]
+	
+	for i in range(1, n):
+		if records[i] > max:
+			max = records[i]
+			
+	index = record.index(max)
+	
+	return max, index
+
+########################  Prepare
+labels=['left', 'rihth', 'go', 'no', 'follow', 'up', 'stop', 'yes', 'down', 'forward']
+	
+X_test = np.load("X_test.npy")
+y_test = np.load("y_test.npy")
+
+tflite_model_file = "speech-model.tflite"
+tflite_quant_model_file = "speech_model_quant.tflite"
+
+X_test = X_test.reshape(X_test.shape[0], MFCC, 51, 1)
+
+sounds = tf.cast(X_test, tf.float32)
+urban_ds = tf.data.Dataset.from_tensor_slices(sounds).batch(1)
+
+
+quant_sounds = tf.quantization.quantize(X_test, X_train_min, X_train_max, tf.quint8, mode="MIN_COMBINED", round_mode="HALF_AWAY_FROM_ZERO", name=None)
+sounds_uint8 = tf.cast(quant_sounds[0], tf.uint8)
+urban_ds_uint8 = tf.data.Dataset.from_tensor_slices(sounds_uint8).batch(1)
+
+###################  Call interpreters
+
+interpreter = tf.lite.Interpreter(model_path=str(tflite_model_file))
+interpreter.allocate_tensors()
+
+interpreter_quant = tf.lite.Interpreter(model_path=str(tflite_quant_model_file ))
+interpreter_quant.allocate_tensors()
+
+####################
+# Inference
+
+for m in range(1, 10):
+	
+	print("\nRunning number ", m)
+	predict_number = m
+	i=0
+	
+	for sound in urban_ds:
+		
+		if i == predict_number:
+			break
+		i = i+1
+	
+	
+	interpreter.set_tensor(interpreter.get_input_details()[0]["index"], sound)
+	interpreter.invoke()
+	predictions = interpreter.get_tensor( interpreter.get_output_details()[0]["index"])
+	
+	print(predictions)
+	
+	class_prediction = predictions.tolist()
+	
+	for record in class_prediction:
+		record
+		
+	records = array(record)
+	
+	n = len(records)
+	
+	class_predicted, indice = predict(records, n)
+	
+	real_words = y_test[predict_number]
+	real_word = int(real_words)
+	word_predicted = labels[indice]
+	
+	print(labels)
+	print("\nFLOATS")
+	print("----------------")
+	print("Predicted class is ", labels[indice])
+	print("Real class is ", labels[real_word])
+	print("-----------------")
+	
+
+
+	i=0
+	
+	for sound in urban_ds_uint8:
+		
+		if i == predict_number:
+			break
+		i = i+1
+	
+	
+	interpreter_quant.set_tensor(interpreter_quant.get_input_details()[0]["index"], sound)
+	interpreter_quant.invoke()
+	predictions = interpreter_quant.get_tensor( interpreter_quant.get_output_details()[0]["index"])
+	
+	print(predictions)
+	
+	class_prediction = predictions.tolist()
+	
+	for record in class_prediction:
+		record
+		
+	records = array(record)
+	
+	n = len(records)
+	
+	class_predicted, indice = predict(records, n)
+	
+	print(predictions)
+	
+
+	print("\nINTEGERS")
+	print("----------------")
+	print("Predicted class is ", labels[indice])
+	print("Real class is ", labels[real_word])
+	print("-----------------")
+	
+	 
+
+
+print("complete")
+
+
